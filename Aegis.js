107c61a39530ee25920424955c08207d7af1ea85
Sachintha Sylva <yohansylva02@gmail.com>
2026-07-08 11:24:12 +0800

    Enhance sign logging with player checks/handling

--- Aegis.js
+++ Aegis.js
@@ -2,7 +2,11 @@
 BlockEvents.placed('minecraft:sign', event => {
     let player = event.player;
     let block = event.block;
-
+    if (player){
     // Logs the player's name and coordinates to the server console
-    console.info(`[Sign Logger] Player ${player.name.getString()} placed a sign at X:${block.x} Y:${block.y} Z:${block.z}`);
+    console.info(`[Aegis - Sign Logger] Player ${player.name.getString()} placed a sign at X:${block.x} Y:${block.y} Z:${block.z}`);
+    }
+    else{
+    console.info('[Aegis - Sign Logger] A sign was placed at X:${block.x} Y:${block.y} Z:${block.z} by an unknown player. \nThis may be due to worldgen or commands placing the sign.')
+    }
 });
